7697f5f3f059dce37452e8cb3c1e90a7b830db86
Sho Sato <rizeupbass@gmail.com>
2020-04-15 15:17:47 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+using Slack.Integration.IncomingWebhook;
+
+namespace SlackWebhook
+{
+    class Program : ConsoleAppBase
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Program>(args);
+        }
+
+        public async Task Run(string url, string text = null, string channel = null, string userName = null)
+        {
+            if (text == null)
+            {
+                Console.Write("Text: ");
+                text = Console.ReadLine();
+            }
+            await new WebhookClient().SendAsync(url, new Payload { Text = text, Channel = channel, UserName = userName });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# SlackWebhook
+
+Simple slack webhook client.
+
+## Install
+
+```
+dotnet tool install -g slack-webhook
+```
+
+## Useage
+
+```
+$ slack-webhook -url https://hooks.slack.com/services/XXXXX/YYYYY/zzzzzzzzzzzzzzzzzz -text "Hello, Slack!"
+$ cat hello.txt | slack-webhook -url https://hooks.slack.com/services/XXXXX/YYYYY/zzzzzzzzzzzzzzzzzz
+$ slack-webhook -url https://hooks.slack.com/services/XXXXX/YYYYY/zzzzzzzzzzzzzzzzzz -text "Hello, Slack!" -channel "#dotnet" -userName "cssho"
+```
